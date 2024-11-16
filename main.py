6ec0d391084ecfa12fcfc74312ec0c9dcac661de
dmitrii.kirkhmeier <dmitrii.kirkhmeier@jetbrains.com>
2024-11-16 19:38:26 +0100

    added solutions to methods

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 class PhoneNumberCheck:
     def parse_block(text_block):
         # your solution goes here
-        return
+        return "solution1"
 
     def verbalize_number(number):
         # your solution goes here
-        return
+        return "solution1"
